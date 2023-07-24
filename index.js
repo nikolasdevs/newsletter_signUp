6a1ed1b5f4093ad1f0306e2e4209c645ad1234fd
Emmanuel Esan <nikolasdevs@gmail.com>
2023-07-24 12:32:03 +0100

    success message was displayed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const form = document.getElementById("form");
 const emailInput = document.getElementById("email");
 const errorLabel = document.getElementById("errorLabel");
+const container = document.getElementById("container");
+const success = document.getElementById("success");
+const dismiss = document.getElementById("dismiss");
 
 function subscribe(event) {
   event.preventDefault();
@@ -12,9 +15,20 @@ function subscribe(event) {
     errorLabel.style.display = "flex";
   } else if (!emailRegex.test(emailValue)) {
     errorLabel.style.display = "flex";
-  }else{
- errorLabel.style.display = "none";
+  } else {
+    errorLabel.style.display = "none";
+    form.reset();
+    container.style.display = "none";
+    success.style.display = "flex";
   }
 }
 
+function reset(event) {
+  event.preventDefault();
+
+  container.style.display = "flex";
+  success.style.display = "none";
+}
+
 form.addEventListener("submit", subscribe);
+dismiss.addEventListener("click", reset);
